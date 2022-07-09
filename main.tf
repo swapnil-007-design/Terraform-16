b428419065f170a525fac7d2db29d19dea1e4486
Swapnil <swapnil.k.ghorude@gmail.com>
2022-07-09 15:39:08 +0530

    --- Auto Git Commit ---

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "name" {
- ami = 
- instance_type =
- key_name =
+ ami = "ami-08df646e18b182346"
+ instance_type = "t2.micro"
+ key_name = "mumbaikey1"
  security_groups = ["defaults"] 
 }
